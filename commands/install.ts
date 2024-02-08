3bdaaf2bc773c1c912f21641409de1496c412a9a
Patrick Christensen <patrick@pat.gg>
2024-02-08 15:01:14 +0100

    Add PowerShell update command

--- commands/install.ts
+++ commands/install.ts
@@ -97,6 +97,32 @@ const installPowershellCommand: CommandModule = {
   }
 }
 
+const updatePowershellCommand: CommandModule = {
+  command: 'pwsh',
+  describe: 'Update powershell',
+  aliases: ['p', 'powershell'],
+  builder: (command: CommandBuilder) => command
+    .example('dotnetman update pwsh'),
+  handler: async (argv: Argv) => {
+    // TODO: Check if `dotnet` is in PATH, error out if not
+
+    const proc = Bun.spawn(['dotnet', 'tool', 'update', '--global', 'powershell'], {
+      stdout: "inherit",
+      stderr: "inherit",
+      env: {
+        ...process.env,
+        DOTNET_ROOT: getDotnetRootPath()
+      }
+    })
+
+    const exitCode = await proc.exited
+
+    if (exitCode !== 0) {
+      throw new Error('Non-zero exit code occurred.')
+    }
+  }
+}
+
 export const installCommand: CommandModule = {
   command: 'install',
   describe: 'Manage installations',
@@ -104,6 +130,14 @@ export const installCommand: CommandModule = {
   builder: (command: CommandBuilder) => command
     .command(installDotnetCommand)
     .command(installPowershellCommand)
+}
+
+export const updateCommand: CommandModule = {
+  command: 'update',
+  describe: 'Update installations',
+  aliases: ['u'],
+  builder: (command: CommandBuilder) => command
+    .command(updatePowershellCommand)
 }
 
 export const sdkCommand: CommandModule = {
--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import yargs from 'yargs/yargs'
-import { installCommand, sdkCommand } from './commands/install.ts'
+import { installCommand, sdkCommand, updateCommand } from './commands/install.ts'
 
 yargs(process.argv.splice(2))
   .scriptName('dotnetman')
   .command(installCommand)
+  .command(updateCommand)
   .command(sdkCommand)
   .help()
   .argv
