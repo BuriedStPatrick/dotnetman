828e263b2a1be701ce201b98db79df2f4b5e73a5
Patrick Christensen <patrick@pat.gg>
2024-02-08 15:30:23 +0100

    Set version from package.json, use macro at build-time

--- index.ts
+++ index.ts
@@ -2,7 +2,10 @@ import yargs from 'yargs/yargs'
 import { installCommand, sdkCommand } from './commands/install.ts'
 import { updateCommand } from './commands/update.ts'
 
+import { getVersion } from './macros/version.ts' with { type: 'macro' };
+
 yargs(process.argv.splice(2))
+  .version(getVersion())
   .scriptName('dotnetman')
   .command(installCommand)
   .command(updateCommand)
